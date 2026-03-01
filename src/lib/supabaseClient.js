c8c2202296adc702a43c9991ecf8092b31795998
Yathindra Mestha <yathindra.mestha@example.com>
2026-03-01 17:39:43 +0530

    Configured Supabase environment variables

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-    console.error("Supabase environment variables are missing!");
-}
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
